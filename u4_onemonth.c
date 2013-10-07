3d364dcfb04c67575ec71000a363ad19e0e28c26
K Kollmann <git@k.kollmann.moe>
2013-10-07 02:01:02 +0200

    added comment re: alignment of numbers

--- u4_onemonth.c
+++ u4_onemonth.c
@@ -10,6 +10,7 @@ Enter starting day of the week (1=Monday, 7=Sunday): 3
 	20	21	22	23	24	25	26
 	27	28	29	30	31
 %
+[numbers are supposed to be righ-aligned]
 
 After the month and day of week have been entered, a single new-line character is printed (see above example, there is a single empty line before the days are printed). Each day printed is followed by a single space character (including those at the end of the week in the 7th column; this also simplifies the implementation). After the last day one space character and a newline character is printed. Ensure that you do not print two new-line characters in case the last day is printed in the 7th column, but exactly one new-line character in all cases after the last day.
 
