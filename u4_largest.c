3421391ff07b0a7df197ace083d0a65ce8ddd1a6
K Kollmann <git@k.kollmann.moe>
2013-10-07 02:24:13 +0200

    replaced tab intendation with spaces

--- u4_largest.c
+++ u4_largest.c
@@ -1,4 +1,4 @@
-/*	Write a program that finds the largest in a series of numbers entered by the user. The program must prompt the user to enter arbitrary many numbers one by one. When the user enters 0 or a negative number, the program must display the largest positive number entered before.
+/*  Write a program that finds the largest in a series of numbers entered by the user. The program must prompt the user to enter arbitrary many numbers one by one. When the user enters 0 or a negative number, the program must display the largest positive number entered before.
 
 % u4_largest
 Enter a number: 50
@@ -18,22 +18,22 @@ No number was entered.
 
 int main() {
 
-	double userinput = 0;
-	double largestnumber = 0;
+    double userinput = 0;
+    double largestnumber = 0;
 
-	do {
-		printf("Enter a number: ");
-		scanf("%lf", &userinput);
-		if(largestnumber < userinput) {
-			largestnumber = userinput;
-		}
-	} while(userinput > 0);
+    do {
+        printf("Enter a number: ");
+        scanf("%lf", &userinput);
+        if(largestnumber < userinput) {
+            largestnumber = userinput;
+        }
+    } while(userinput > 0);
 
-	if(largestnumber > 0) {
-		printf("\nThe largest number entered was %.2lf\n", largestnumber);
-	} else {
-		printf("\nNo number was entered.\n");
-	}
+    if(largestnumber > 0) {
+        printf("\nThe largest number entered was %.2lf\n", largestnumber);
+    } else {
+        printf("\nNo number was entered.\n");
+    }
 
-	return 0;
+    return 0;
 }
