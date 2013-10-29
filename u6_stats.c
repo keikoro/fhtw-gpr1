5ae3b1dc5daa24a0cc9a4aa84c635c5970a58633
K Kollmann <git@k.kollmann.moe>
2013-10-29 01:36:36 +0100

    renamed u6_stats_new

--- u6_stats.c
+++ u6_stats.c
@@ -18,111 +18,103 @@ NOT VALID
 #include <stdlib.h>
 #include <string.h>
 
-
 int main() {
 
-    char sentence[82] = {'\0'};
-    int wordlengths[2][11] = {}; // max. 11 diff. word lengths in 79 chars
+    /*  Allow up to 82 chars so "NOT VALID" can be triggered on inputs with more than 81 chars (= 80 allowed characters + 1 null terminator).    */
+    int maxchars = 82;
+    char userinput[82] = {'\0'};
+    //  max. 11 diff. possible word lengths in 80 chars (w/ spaces inbetween)
+    int wordlengths[2][11] = {};
     int countchars = 0;
-    int newlength = 0;
-    int valid = 1;
-    int i=0, j=0, k=0, l=0;
-    int temp0 = 0, temp1 = 0;
+    int thislength = 0;
+    int validletter = 0;
+    int placeholder_0;
+    int placeholder_1;
+    int i=0, j=0, k=0;
 
     printf("Enter a sentence: \n");
-
-    // fgets reads full sentences from standard input
-    // allowing 82 chars to be able to sort out inputs that are too long
-    fgets(sentence, 82, stdin);
-    /*  fgets includes newline character (for Enter) if input is less than
-        the allowed number of characters (i.e. only if there's space left in
-        the array). -> Check for '\n' and replace it with null terminator if it exists so it doesn't get counted toward character count. */
-    for (i=0;i<82;i++) {
-        if(sentence[i] == '\n') {
-            sentence[i] = '\0';
+    /*  fgets reads full userinputs from standard input including spaces, special chars and - if input is less than the allowed no. of character (= if there's space left in the array) - also new line characters (Enter key).  */
+    fgets(userinput, maxchars, stdin);
+    /*  As newline characters are also counted with strlen(), a potential '\n' character needs to be replaced with the null terminator.  */
+    for (i=0;i<maxchars;i++) {
+        if(userinput[i] == '\n') {
+            userinput[i] = '\0';
             break;
         }
     }
-    countchars = strlen(sentence);
-    // printf("character count: %d\n", countchars);
+    countchars = strlen(userinput); // all printable chars + spaces
 
-    if((countchars > 0) && (countchars <= 80)) {    // expected input
+    // input is at least one character and not more than 80
+    if(countchars > 0 && countchars <= 80) {
+        // go through all characters of userinput
         for(i=0;i<countchars;i++) {
-            if(sentence[i] == '.' || sentence[i] == ' ') {
-                if (j > 0) {    // length of preceding word
-                    // check array for word length j
-                    for(k=0;k<11;k++) {
-                        if (wordlengths[0][k] == j) {
-                            wordlengths[1][k] += 1;
-                            newlength = 1;  // word length already in array
-                            break;
-                        }
+            // check if userinput[i] is a lowercase character a-z
+            for(char ch='a';ch<='z';ch++) {
+                if(userinput[i] == ch) {
+                    thislength++;
+                    validletter = 1;
+                }
+            }
+            // if userinput[i] isn't lowercase, check if it's uppercase A-Z
+            if (validletter != 1) {
+                for(char ch='A';ch<='Z';ch++) {
+                    if(userinput[i] == ch) {
+                        thislength++;
+                        validletter = 1;
                     }
-                    // if word length doesn't exist yet, add it
-                    if (newlength == 0) {
-                        for(k=0;k<11;k++) { // find empty column
-                            if (wordlengths[0][k] == 0) {
-                                wordlengths[0][k] = j;
-                                wordlengths[1][k] = 1;
+                }
+            }
+            // userinput[i] is no character at all; check for spaces and '.'
+            if(validletter != 1) {
+                if(userinput[i] == ' ' || userinput[i] == '.') {
+                    if (thislength > 0) {
+                        // check if word length already in array
+                        for (j=0;j<11;j++) {
+                            if (wordlengths[0][j] == thislength) {  // yes
+                                wordlengths[1][j] += 1;
+                                thislength = 0;
                                 break;
                             }
                         }
+                        if (thislength != 0) {
+                            for (j=0;j<11;j++) {    // no; add to array
+                                if (wordlengths[0][j] == 0) {
+                                    wordlengths[0][j] = thislength;
+                                    wordlengths[1][j] += 1;
+                                    thislength = 0;
+                                    break;
+                                }
+                            }
+                        }
                     }
-                }
-                j = 0;  // reset word length to zero
-                newlength = 0;  // reset indicator for new word length
-
-                if(sentence[i] == '.') {
-                    break;
-                }
-
-            } else {
-                // check for letters A-Z (GPR course materials, unit4, p10)
-                for(char ch='A'; ch<='Z'; ch++) {
-                    if (sentence[i] == ch) {
-                        j++;
-                        break;
-                    }
-                }
-                for(char ch='a'; ch<='z'; ch++) {
-                    if (sentence[i] == ch) {
-                        j++;
-                        break;
-                    }
-                }
-                if (j == 0) {   // sentence[i] is not a valid letter
+                } else {    // userinput[i] is no valid character -> exit
                     printf("NOT VALID\n");
-                    valid = 0;
-                    break;
+                    exit(0);
+                }
+            }
+            validletter = 0;
+        }
+        // sort array by word length ascending (bubble sort)
+        for(j=1;j<11;j++) {
+            for(k=1;k<11;k++) {
+                if(wordlengths[0][k] < wordlengths[0][k-1]) {
+                    placeholder_0 = wordlengths[0][k-1];
+                    placeholder_1 = wordlengths[1][k-1];
+                    wordlengths[0][k-1] = wordlengths[0][k];
+                    wordlengths[1][k-1] = wordlengths[1][k];
+                    wordlengths[0][k] = placeholder_0;
+                    wordlengths[1][k] = placeholder_1;
                 }
             }
         }
-        // only output word lengths in array if ALL input was valid
-        if (valid == 1) {
-
-            // sort by word length ascending
-            for(l=1;l<11;l++) {
-                for(k=1;k<11;k++) {
-                    if(wordlengths[0][k] < wordlengths[0][k-1]) {
-                        temp0 = wordlengths[0][k-1];
-                        temp1 = wordlengths[1][k-1];
-                        wordlengths[0][k-1] = wordlengths[0][k];
-                        wordlengths[1][k-1] = wordlengths[1][k];
-                        wordlengths[0][k] = temp0;
-                        wordlengths[1][k] = temp1;
-                    }
-                }
-            }
-
-            for(k=0;k<11;k++) {
-                if (wordlengths[0][k] != 0) {   // ignore empty columns
-                    printf("Length %d: %d\n", wordlengths[0][k], wordlengths[1][k]);
-                }
+        for(k=0;k<11;k++) {
+            if (wordlengths[0][k] != 0) {   // ignore empty columns
+                printf("Length %d: %d\n", wordlengths[0][k], wordlengths[1][k]);
             }
         }
-    } else if (countchars == 0) {   // no input at all
-    } else if (countchars > 79) {   // more characters than allowed
+    } else if (countchars > 80) {   // more than allowed characters entered
         printf("NOT VALID\n");
+    } else {   // no characters entered - do nothing
     }
     return 0;
 }
