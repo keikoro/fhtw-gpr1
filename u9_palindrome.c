98349da5bb8eb9d97515039b3105fcdeb3b27240
K Kollmann <git@k.kollmann.moe>
2013-11-09 02:58:57 +0100

    check against ASCII characters for letters, print negative message for empty letter array or only one letter

--- u9_palindrome.c
+++ u9_palindrome.c
@@ -24,12 +24,14 @@ int main() {
 
     char message[maxchars] = {'\0'};
     int countmsg = 0;
+    char letters[maxchars] = {'\0'};
+    int countletters = 0;
+    char specialchars[maxchars] = {'\0'};
     // int bla[] = {1,2,3};
     // int (*b)[] = &bla;
     char (*p)[maxchars] = &message;
-    int i = 0;
+    int i=0, j=0, k=0;
 
-    printf("palindrome\n");
     printf("Enter a message: ");
     fgets(message, maxchars, stdin);
 
@@ -43,11 +45,38 @@ int main() {
     countmsg = strlen(message);
 
     /*  STEPS
-        separate the input into alphabetical and non-alphabetical characters, put them into two separate arrays (use ASCII instead of checking against all letters!*)
+        separate the input into alphabetical and non-alphabetical characters, put them into two separate arrays. Use ASCII instead of checking against all letters!
 
+        ASCII table
+        A-Z ... 65-90
+        a-z ... 97-122
+    */
+
+    for(i=0;i<countmsg;i++) {
+
+        if( (message[i] >= 65 && message[i] <= 90) || (message[i] >= 97 && message[i] <= 122) ) {
+            letters[j] = message[i];
+            j++;
+        }
+
+    }
+    countletters = strlen(letters);
+    printf("number of letters: %d\n", countletters);
+
+
+    /*
         if both arrays are empty -> palindrome
         if the array with alphabetic chars is empty -> palindrome
 
+    */
+
+    if(countletters > 1) {
+
+    } else {
+        printf("Not a palindrome\n");
+    }
+
+    /*
         if the array with alphabetic chars is not empty:
         adjust countmsg (count length of array)
 
@@ -57,9 +86,7 @@ int main() {
         - divide countmsg by two, save result in variable looptimes
         - check the first letter against last, 2nd against one but last etc., so (*p)[0] vs. (*p)[countmsg-1], (*p)[1] vs. (*p)[countmsg-2] => (*p)[i] vs. (*p)[countmsg-i-1] looptimes times
 
-        *ASCII table
-        A-Z ... 65-90
-        a-z ... 97-122
+
     */
 
     printf("message length: %d\n", countmsg);
